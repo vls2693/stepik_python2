cc540b401ce7bb4f7424fdd474470e9569a375a1
vls <belyakov38@gmail.com>
2018-06-27 15:16:52 +0300

    Initial commit

---
+++ rr.py
@@ -0,0 +1,27 @@
+'''
+9
+add global a
+create foo global
+add foo b
+get foo a
+get foo c
+create bar foo
+add bar a
+get bar a
+get bar b
+'''
+full = []
+pad = input()
+full.append(pad)
+
+while True:
+    pad = input()
+    d = pad.split(' ')
+    if not pad:
+        break
+    full.append(d)
+
+for i in full:
+    if len(i) != 3:
+        full.remove(i)
+print(full)
