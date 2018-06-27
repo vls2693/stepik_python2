d76c43f6c2f480792907a90f9be8c18d18c38312
vls <belyakov38@gmail.com>
2018-06-27 17:38:32 +0300

    Python: основы и применение задание 1.4 стадия присоединения элементов к списку global_env

--- rr.py
+++ rr.py
@@ -29,4 +29,38 @@ global_env = []
 
 for i in full:
     if i[0] != "get":
-        print(i)
+        if i[1] == "global":
+            global_env.append(i[2])
+        if i[2] == "global":
+            global_env.append(i[1])
+
+
+print(global_env)
+
+for l in range(0, len(full)):
+    for i in full:
+        if i[0] != "get":
+            if i[1] == 'global':
+                full.remove(i)
+            elif i[2] == 'global':
+                full.remove(i)
+            else:
+                continue
+        else:
+            continue
+
+print(full)
+exist = []
+for i in range(0, len(global_env)):
+    exist.append('x')
+print(exist)
+for i in range(0, 1):
+    if full[i][1] in global_env:
+        print(global_env.index(full[i][1]))
+        print(global_env[global_env.index(full[i][1])][0])
+        print(global_env[global_env.index(full[i][1])][1])
+        print(global_env[global_env.index(full[i][1])][2])
+
+for i in range(0, len(exist)):
+    exist[i].append('r')
+print(exist)
